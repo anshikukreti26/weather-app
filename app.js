e812ae3884b79f45a7acb59dec0ec2056efb78b5
Anshika Kukreti <anshikukreti26@gmail.com>
2024-02-14 01:34:46 +0530

    fetch current weather

--- app.js
+++ app.js
@@ -11,4 +11,15 @@ function getWeather() {
     const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
+    fetch(currentWeatherUrl)
+    .then(response => response.json())
+    .then(data => {
+        displayWeather(data);
+    })
+    .catch(error => {
+        console.error('Error fetching current weather data:', error);
+        alert('Error fetching current weather data. Please try again.');
+    });
+
+    
 }
